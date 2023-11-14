3bb7110ad581ce762a6dfa50ed92986600c8db36
soyeong_jung <affectionmej@gmail.com>
2023-11-14 14:51:17 +0900

    fullcalendar 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -19,6 +19,14 @@ var main = {
             _this.save_cmt();
         });
 
+        //fullcalendar
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+              initialView: 'dayGridMonth'
+            });
+            calendar.render();
+        });
     },
 
     save : function () {
